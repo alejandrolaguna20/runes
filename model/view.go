c7e6328f91ea2905202c50713d7a4b118bf6ceb4
Alejandro Laguna <alejandro.laguna@epitech.eu>
2025-06-17 11:15:45 +0200

    feat: proper centered content

--- model/view.go
+++ model/view.go
@@ -3,17 +3,28 @@ package model
 import "github.com/charmbracelet/lipgloss"
 
 func (m Model) View() string {
-	style := lipgloss.NewStyle().
-		BorderStyle(lipgloss.NormalBorder()).
-		Padding(2, 2).
-		Align(lipgloss.Center)
-	content := style.Render("hello, runes")
-	centeredContent := lipgloss.Place(
+	mainContent := lipgloss.NewStyle().
+		BorderStyle(lipgloss.RoundedBorder()).
+		Padding(2, 4).
+		Align(lipgloss.Center).
+		Render("hello, runes")
+
+	bottomText := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("241")).
+		Render("Press q to quit")
+
+	combined := lipgloss.JoinVertical(
+		lipgloss.Center,
+		mainContent,
+		"",
+		bottomText,
+	)
+
+	return lipgloss.Place(
 		m.viewport.Width,
 		m.viewport.Height,
 		lipgloss.Center,
 		lipgloss.Center,
-		content,
+		combined,
 	)
-	return centeredContent
 }
